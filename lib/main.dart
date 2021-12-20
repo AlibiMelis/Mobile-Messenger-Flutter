9a4c9315275cf65524a1c2736b9deab65e598b7a
Alibi Melis <melis.alibi2000@gmail.com>
2021-12-20 14:36:16 +0800

    Built lsit and chat views

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_messenger_flutter/screens/home_screen.dart';
 
 void main() {
-  runApp(const MaterialApp());
-  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = ThemeData(
+      primarySwatch: Colors.red,
+    );
+
+    return MaterialApp(
+      title: 'Mobile Messenger Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          secondary: const Color(0xFFFEF9EB),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
 }
